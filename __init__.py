4144c91e87b2c3da49a1036ec253801779da0405
Justin <justin.seiser@rackspace.com>
2019-05-29 15:32:09 -0400

    seeing if  we can format messages to slack

--- __init__.py
+++ __init__.py
@@ -6,15 +6,11 @@ import aiohttp
 
 class AWXSkill(Skill):
     async def _get_inventories(self, environment):
-        print(self.config["sites"][environment]["username"])
-        print(self.config["sites"][environment]["password"])
         auth = aiohttp.BasicAuth(
             login=self.config["sites"][environment]["username"],
             password=self.config["sites"][environment]["password"],
         )
-        # api_url = self.config["sites"][environment]["url"] + "/api/v2/inventories/"
         api_url = f"{self.config['sites'][environment]['url']}/api/v2/inventories/"
-        print(api_url)
 
         async with aiohttp.ClientSession(auth=auth) as session:
             async with session.get(api_url) as resp:
@@ -28,6 +24,8 @@ class AWXSkill(Skill):
     async def list_inventory(self, message):
         environment = message.regex.group("environment")
         inventories = await self._get_inventories(environment)
-        print(inventories)
-
-        await message.respond(f"{inventories}")
+        data = {
+            "text": "I am a test message http://slack.com",
+            "attachments": [{"text": "And here’s an attachment!"}],
+        }
+        await message.respond(data)
