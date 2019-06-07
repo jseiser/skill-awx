d7eecfc27da5137b329a8a6a4fa640ee383c4519
Justin <justin.seiser@rackspace.com>
2019-06-07 10:47:19 -0400

    moving session stuff to functions

--- __init__.py
+++ __init__.py
@@ -6,104 +6,73 @@ import datetime
 
 
 class AWXSkill(Skill):
-    async def _get_inventories(self, deployment):
+    async def _rest_call(self, deployment, api_url, call_method):
         auth = aiohttp.BasicAuth(
             login=self.config["sites"][deployment]["username"],
             password=self.config["sites"][deployment]["password"],
         )
         timeout = aiohttp.ClientTimeout(total=60)
-        api_url = f"{self.config['sites'][deployment]['url']}/api/v2/inventories/"
-
         async with aiohttp.ClientSession(auth=auth, timeout=timeout) as session:
-            async with session.get(api_url) as resp:
-                return_text = f"*{deployment} - Inventories*\n"
-                data = await resp.json()
-                for i in data["results"]:
-                    return_text = (
-                        f"{return_text}```ID: {i['id']} Name: {i['name']}```\n"
-                    )
-                return return_text
+            if call_method == "get":
+                async with session.get(api_url) as resp:
+                    data = await resp.json()
+                    return data
+            else:
+                async with session.post(api_url) as resp:
+                    data = await resp.json()
+                    return data
+
+    async def _get_inventories(self, deployment):
+        api_url = f"{self.config['sites'][deployment]['url']}/api/v2/inventories/"
+        data = await self._rest_call(deployment, api_url, "get")
+        return_text = f"*{deployment} - Inventories*\n"
+        for i in data["results"]:
+            return_text = f"{return_text}```ID: {i['id']} Name: {i['name']}```\n"
+        return return_text
 
     async def _update_inventory(self, deployment, inventory):
-        auth = aiohttp.BasicAuth(
-            login=self.config["sites"][deployment]["username"],
-            password=self.config["sites"][deployment]["password"],
-        )
-        timeout = aiohttp.ClientTimeout(total=60)
         api_url = f"{self.config['sites'][deployment]['url']}/api/v2/inventories/{inventory}/update_inventory_sources/"
-
-        async with aiohttp.ClientSession(auth=auth, timeout=timeout) as session:
-            async with session.post(api_url) as resp:
-                return_text = f"*{deployment} - Inventory Update* \n"
-                data = await resp.json()
-                result = data[0]
-                return_text = f"{return_text}```Status: {resp.status} State: {result['status']}```"
-                return return_text
+        data = await self._rest_call(deployment, api_url, post)
+        return_text = f"*{deployment} - Inventory Update* \n"
+        result = data[0]
+        return_text = f"{return_text}```State: {result['status']}```"
+        return return_text
 
     async def _update_project(self, deployment, id):
-        auth = aiohttp.BasicAuth(
-            login=self.config["sites"][deployment]["username"],
-            password=self.config["sites"][deployment]["password"],
-        )
-        timeout = aiohttp.ClientTimeout(total=60)
         api_url = (
             f"{self.config['sites'][deployment]['url']}/api/v2/projects/{id}/update/"
         )
 
-        async with aiohttp.ClientSession(auth=auth, timeout=timeout) as session:
-            async with session.post(api_url) as resp:
-                return_text = f"*{deployment} - Project Update* \n"
-                data = await resp.json()
-                result = data
-                return_text = f"{return_text}```Status: {resp.status} State: {result['status']}```"
-                return return_text
+        return_text = f"*{deployment} - Project Update* \n"
+        result = await self._rest_call(deployment, api_url, post)
+        return_text = f"{return_text}```State: {result['status']}```"
+        return return_text
 
     async def _get_projects(self, deployment):
-        auth = aiohttp.BasicAuth(
-            login=self.config["sites"][deployment]["username"],
-            password=self.config["sites"][deployment]["password"],
-        )
-        timeout = aiohttp.ClientTimeout(total=60)
         api_url = f"{self.config['sites'][deployment]['url']}/api/v2/projects/"
-
-        async with aiohttp.ClientSession(auth=auth, timeout=timeout) as session:
-            async with session.get(api_url) as resp:
-                data = await resp.json()
-                if data["count"] > 0:
-                    return_text = f"*{deployment} - Projects*\n"
-                    for i in data["results"]:
-                        return_text = f"{return_text}```ID: {i['id']}\nName: {i['name']}\nscm_url: {i['scm_url']}\nscm_branch: {i['scm_branch']}```\n"
-                else:
-                    return_text = f"*{deployment} - No Projects*"
-                return return_text
+        data = await self._rest_call(deployment, api_url, "get")
+        if data["count"] > 0:
+            return_text = f"*{deployment} - Projects*\n"
+            for i in data["results"]:
+                return_text = f"{return_text}```ID: {i['id']}\nName: {i['name']}\nscm_url: {i['scm_url']}\nscm_branch: {i['scm_branch']}```\n"
+        else:
+            return_text = f"*{deployment} - No Projects*"
+        return return_text
 
     async def _get_running_jobs(self, deployment):
-        auth = aiohttp.BasicAuth(
-            login=self.config["sites"][deployment]["username"],
-            password=self.config["sites"][deployment]["password"],
-        )
-        timeout = aiohttp.ClientTimeout(total=60)
         api_url = (
             f"{self.config['sites'][deployment]['url']}/api/v2/jobs/?status=running"
         )
-
-        async with aiohttp.ClientSession(auth=auth, timeout=timeout) as session:
-            async with session.get(api_url) as resp:
-                data = await resp.json()
-                if data["count"] > 0:
-                    return_text = f"*{deployment} - Running Jobs*\n"
-                    for i in data["results"]:
-                        return_text = f"{return_text}```Date: {i['started']} ID: {i['id']} Name: {i['name']} Playbook: {i['playbook']}```\n"
-                else:
-                    return_text = f"*{deployment} - No Running Jobs*"
-                return return_text
+        data = await self._rest_call(deployment, api_url, "get")
+        if data["count"] > 0:
+            return_text = f"*{deployment} - Running Jobs*\n"
+            for i in data["results"]:
+                return_text = f"{return_text}```Date: {i['started']} ID: {i['id']} Name: {i['name']} Playbook: {i['playbook']}```\n"
+        else:
+            return_text = f"*{deployment} - No Running Jobs*"
+        return return_text
 
     async def _get_failed_jobs(self, deployment, num=5, yesterday=None):
-        auth = aiohttp.BasicAuth(
-            login=self.config["sites"][deployment]["username"],
-            password=self.config["sites"][deployment]["password"],
-        )
-        timeout = aiohttp.ClientTimeout(total=60)
         if yesterday:
             yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
             start_time = yesterday.isoformat()
@@ -111,56 +80,40 @@ class AWXSkill(Skill):
         else:
             api_url = f"{self.config['sites'][deployment]['url']}/api/v2/jobs/?status=failed&order_by=-started&page_size={num}"
 
-        async with aiohttp.ClientSession(auth=auth, timeout=timeout) as session:
-            async with session.get(api_url) as resp:
-                data = await resp.json()
-                if data["count"] > 0:
-                    return_text = f"*{deployment} - Failed Jobs*\n"
-                    for i in data["results"]:
-                        return_text = f"{return_text}```Date: {i['started']} ID: {i['id']} Name: {i['name']} Playbook: {i['playbook']}```\n"
-                else:
-                    return_text = f"*{deployment} - No Failed Jobs*"
-                return return_text
+        data = await self._rest_call(deployment, api_url, "get")
+        if data["count"] > 0:
+            return_text = f"*{deployment} - Failed Jobs*\n"
+            for i in data["results"]:
+                return_text = f"{return_text}```Date: {i['started']} ID: {i['id']} Name: {i['name']} Playbook: {i['playbook']}```\n"
+        else:
+            return_text = f"*{deployment} - No Failed Jobs*"
+        return return_text
 
     async def _get_scheduled_jobs(self, deployment, num=5):
-        auth = aiohttp.BasicAuth(
-            login=self.config["sites"][deployment]["username"],
-            password=self.config["sites"][deployment]["password"],
-        )
-        timeout = aiohttp.ClientTimeout(total=60)
         api_url = f"{self.config['sites'][deployment]['url']}/api/v2/schedules/?enabled=true&order_by=next_run&page_size={num}"
 
-        async with aiohttp.ClientSession(auth=auth, timeout=timeout) as session:
-            async with session.get(api_url) as resp:
-                data = await resp.json()
-                if data["count"] > 0:
-                    return_text = f"*{deployment} - Next 5 Scheduled Jobs*\n"
-                    for i in data["results"]:
-                        return_text = f"{return_text}```Next Run: {i['next_run']} ID: {i['id']} Name: {i['name']}```\n"
-                else:
-                    return_text = f"*{deployment} - No Scheduled Jobs*"
-                return return_text
+        data = await self._rest_call(deployment, api_url, "get")
+        if data["count"] > 0:
+            return_text = f"*{deployment} - Next {num} Scheduled Jobs*\n"
+            for i in data["results"]:
+                return_text = f"{return_text}```Next Run: {i['next_run']} ID: {i['id']} Name: {i['name']}```\n"
+        else:
+            return_text = f"*{deployment} - No Scheduled Jobs*"
+        return return_text
 
     async def _get_scheduled_jobs_past(self, deployment):
-        auth = aiohttp.BasicAuth(
-            login=self.config["sites"][deployment]["username"],
-            password=self.config["sites"][deployment]["password"],
-        )
-        timeout = aiohttp.ClientTimeout(total=60)
         today = datetime.datetime.now()
         next_run = today.isoformat()
         api_url = f"{self.config['sites'][deployment]['url']}/api/v2/schedules/?enabled=true&order_by=next_run&next_run__lt={next_run}&page_size=20"
 
-        async with aiohttp.ClientSession(auth=auth, timeout=timeout) as session:
-            async with session.get(api_url) as resp:
-                data = await resp.json()
-                if data["count"] > 0:
-                    return_text = f"*{deployment} - Past Scheduled Jobs*\n"
-                    for i in data["results"]:
-                        return_text = f"{return_text}```Next Run: {i['next_run']} ID: {i['id']} Name: {i['name']}```\n"
-                else:
-                    return_text = f"*{deployment} - No Scheduled Jobs*"
-                return return_text
+        data = await self._rest_call(deployment, api_url, "get")
+        if data["count"] > 0:
+            return_text = f"*{deployment} - Past Scheduled Jobs*\n"
+            for i in data["results"]:
+                return_text = f"{return_text}```Next Run: {i['next_run']} ID: {i['id']} Name: {i['name']}```\n"
+        else:
+            return_text = f"*{deployment} - No Scheduled Jobs*"
+        return return_text
 
     async def _get_deployments(self):
         sites = self.config["sites"]
